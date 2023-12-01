790f87096bf1691bcc0b59e1b1db72df57a507f5
Sparrow696 <104690230+Sparrow696@users.noreply.github.com>
2023-12-01 23:47:58 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -10,30 +10,18 @@
 int main()
 {
 	setlocale(LC_ALL, "Rus");
-	unsigned int number = 0;
-	unsigned int number2 = 0;
-	unsigned int sqr = 0;
-	int t = 0;
-	int rem = 0;
-	int d = 1;
-	printf("число: ");
-	scanf_s("%d", &number);
-	while (number != 0) {
-		rem = number % 8;
-		number2 = number2 + rem * d;
-		number /= 8;
-		d *= 10;
-	}
-	number2 /= 10;
-	printf("%d",number2);
-	for (unsigned i = 1; sqr + 2 <= number2; i += 2) {
-		sqr += i;
-		t++;
-	}
-	if (number2 == sqr || number2 == 1)
+	char line[100] = "";
+
+	FILE* file;
+	fopen_s(&file, "file.txt", "r");
+	if (!file)
 	{
-		printf("Yes");
+		perror("f == NULL!");
+		return 1;
 	}
+	printf("%d\n", FGetLine(line, file));
+	printf("%s\n", line);
+	fclose(file);
 
 	return 0;
 }
